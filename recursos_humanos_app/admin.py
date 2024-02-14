993a3bc05cc89444c770c5f6ce296eac4548c9fe
Ossuan <ossuan.lugo@gmail.com>
2024-02-14 13:58:59 -0500

    estructura completa y filtro por ci

--- recursos_humanos_app/admin.py
+++ recursos_humanos_app/admin.py
@@ -3,6 +3,11 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Direccion)
-admin.site.register(Trabajador)
+
+@admin.register(Trabajador)
+class TrabajadorAdmin(admin.ModelAdmin):
+    list_display = ["carnet_identidad", "nombre"]
+    search_fields = ["carnet_identidad", "nombre"]
+
 admin.site.register(TrabajadorNoDocente)
 admin.site.register(TrabajadorDocente)
