b77d9e6967d7230293cea721d86e92de5833c8fe
Ichigo <moralesfloresmaranthony@gmail.com>
2024-02-14 14:24:34 -0500

    listar trabajadores

--- recursos_humanos_app/views.py
+++ recursos_humanos_app/views.py
@@ -7,3 +7,14 @@ def crearTrabajador(request):
 
 def eliminarTrabajador(request):
     return render(request, "eliminarTrabajador.html")   
+
+def listar_trabajadores(request):
+    tipo_trabajador = request.GET.get('tipo_trabajador', None)
+    trabajadores = []
+
+    if tipo_trabajador == 'docente':
+        trabajadores = TrabajadorDocente.objects.all()
+    elif tipo_trabajador == 'no_docente':
+        trabajadores = TrabajadorNoDocente.objects.all()
+
+    return render(request, 'listarTrabajadores.html', {'trabajadores': trabajadores})
--- suficiencia/urls.py
+++ suficiencia/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.crearTrabajador),
     path('eliminar/', views.eliminarTrabajador),
+    path('lista/', views.listar_trabajadores),
 ]
