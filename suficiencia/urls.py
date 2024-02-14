22662daf8691d3128e5e01ade816ac53590db5aa
Ichigo <moralesfloresmaranthony@gmail.com>
2024-02-14 14:06:23 -0500

    form de crear usuario-trabajador

--- suficiencia/urls.py
+++ suficiencia/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recursos_humanos_app import views
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', views.home),
 ]
